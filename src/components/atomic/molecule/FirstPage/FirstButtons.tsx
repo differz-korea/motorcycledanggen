cada91269d695e09e96c2dcc899e25f7b11e1955
Haneul0122 <kjsw1566@gmail.com>
2023-11-29 18:23:51 +0900

    a

--- src/components/atomic/molecule/FirstPage/FirstButtons.tsx
+++ src/components/atomic/molecule/FirstPage/FirstButtons.tsx
@@ -1,19 +1,51 @@
-import React from 'react'
-import FirstpageButton from '../../atom/firstPageButton/FirstPageButton';
+import React from "react";
+import FirstpageButton from "../../atom/firstPageButton/FirstPageButton";
 
-type Props = {}
+type Props = {};
 
 const FirstButtons = (props: Props) => {
   return (
-    <div className='flex flex-col items-center gap-5'>
-      < FirstpageButton title={"수리하기"} subtitle={"최저가 수리비 알아보기"} img={"/assets/img/repair-tool.svg"} alt={"string"} height={true} subtitle2={''} />
-      <div className='flex flex-row  gap-5 w-full'>
-        < FirstpageButton title={"판매하기"} subtitle={"쉽고 빠르게 진행되는"} img={"/assets/img/coin2.svg"} alt={"string"} height={true} subtitle2={'오토바이 판매!'} />
-        < FirstpageButton title={"구매하기"} subtitle={"구매부터 탁송까지"} img={"/assets/img/fast-delivery.svg"} alt={"string"} height={true} subtitle2={'한번에!'} />
+    <div className="flex flex-col items-center gap-5">
+      <FirstpageButton
+        title={"수리하기"}
+        subtitle={"최저가 수리비 알아보기"}
+        img={"/assets/img/repair-tool.svg"}
+        alt={"string"}
+        height={true}
+        subtitle2={""}
+        link={"/repair"}
+      />
+      <div className="flex flex-row  gap-5 w-full">
+        <FirstpageButton
+          title={"판매하기"}
+          subtitle={"쉽고 빠르게 진행되는"}
+          img={"/assets/img/coin2.svg"}
+          alt={"string"}
+          height={true}
+          subtitle2={"오토바이 판매!"}
+          link={""}
+        />
+        <FirstpageButton
+          title={"구매하기"}
+          subtitle={"구매부터 탁송까지"}
+          img={"/assets/img/fast-delivery.svg"}
+          alt={"string"}
+          height={true}
+          subtitle2={"한번에!"}
+          link={""}
+        />
       </div>
-      < FirstpageButton title={"홈으로"} subtitle={"배추딜러의 다양한 서비스를 만나보세요!"} img={""} alt={""} height={false} subtitle2={''} />
+      <FirstpageButton
+        title={"홈으로"}
+        subtitle={"배추딜러의 다양한 서비스를 만나보세요!"}
+        img={""}
+        alt={""}
+        height={false}
+        subtitle2={""}
+        link={"/home"}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default FirstButtons
+export default FirstButtons;
